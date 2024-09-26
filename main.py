b3a0d7c6361de2f6302e81fa00c858bdd74b81d8
staffgha <a@fakeemail.com>
2024-09-26 10:11:51 +0100

    workng background

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-#fjklhsdfjklhg
-print("goodbye")
+import pygame
+import constants
+
+window = pygame.display.set_mode((constants.WIDTH,constants.HEIGHT))
+
+window.fill(constants.MENU_BACKGROUND)
+running = True
+while running:
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.display.flip()
