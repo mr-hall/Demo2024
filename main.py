b6a0f6ab352d5e1f7f0ccab1497818ac9c8cfb23
staffgha <a@fakeemail.com>
2024-09-26 10:25:11 +0100

    different states

--- main.py
+++ main.py
@@ -2,18 +2,29 @@ import pygame
 import constants
 import states
 
+#initialising
 window = pygame.display.set_mode((constants.WIDTH,constants.HEIGHT))
 running = True
 current_state = states.Menu()
 clock = pygame.time.Clock()
+
+#game loop
 while running:
+
+    #handling events
     events = pygame.event.get()
     for event in events:
         if event.type == pygame.QUIT:
             running = False
         else:
             current_state.input(event)
+
+    #updates
     current_state.update()
+
+    #drawing
     current_state.render(window)
     pygame.display.flip()
+
+    #clock tick
     clock.tick(constants.FPS)
--- states.py
+++ states.py
@@ -2,6 +2,7 @@ import pygame
 import constants
 
 class State():
+    #parent class for states default behaviour
     def __init__(self):
         pass
 
@@ -14,13 +15,16 @@ class State():
     def render(self, window):
         pass
 class Menu(State):
+    #main menu state
     def __init__(self):
         super().__init__()
 
     def input(self, event):
+        #handles key presses
         if event.type == pygame.KEYDOWN:
             pass
 
     def render(self, window):
+        #draw frame
         window.fill(constants.MENU_BACKGROUND)
 
